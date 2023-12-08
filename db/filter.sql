abaa4d48460f922c131d4361f082121d59cdecef
s-nt-s <santos82h@gmail.com>
2023-12-08 18:12:50 +0100

    filtro

--- db/filter.sql
+++ db/filter.sql
@@ -1,8 +1,17 @@
 PRAGMA foreign_keys = ON;
 delete from CENTRO where 
-    titularidad!='PUB' and not exists(
-    select * from CONCURSO_ANEXO_CENTRO cac where cac.centro=CENTRO.id
-);
+    not exists(select * from CONCURSO_ANEXO_CENTRO cac where cac.centro=CENTRO.id)
+    AND (
+        titularidad!='PUB' OR
+        titular!='COMUNIDAD DE MADRID' OR
+        tipo in (
+            '300', -- ESTABLECIMIENTO ADMINISTRATIVO
+            '303'  -- CENTRO DOCENTE EN EL EXTRANJERO
+        ) -- OR
+        -- not exists(select * from centro c where c.titular='COMUNIDAD DE MADRID' and c.tipo=CENTRO.tipo)
+        -- (tipo not in ('0', '00', '000') and printf("%d", tipo)='0')
+    )
+;
 
 delete from ETAPA where not exists (
     select * from ETAPA_CENTRO ec where ec.etapa=ETAPA.id
