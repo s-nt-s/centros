e82b960df57855c65b85f5b651f65513464f4406
s-nt-s <santos82h@gmail.com>
2024-06-13 20:07:50 +0200

    reseolver error https://supabase.com/docs/guides/database/database-advisors?queryGroups=lint&lint=0010_security_definer_view

--- db/set_db.sh
+++ db/set_db.sh
@@ -24,15 +24,15 @@ function mk_schema () {
     if [ "$1" == "public" ]; then
         for c in $(sqlite3 db.sqlite 'select id from concurso'); do
             cc=$(echo "$c" | sed 's/-/_/g')
-            echo "  DO \$\$ CREATE VIEW ${cc}_ctr AS (select distinct centro as id from concurso_anexo_centro where concurso='${c}' order by centro); \$\$" >> "aux/config.load"
-            echo "  DO \$\$ CREATE VIEW ${cc}_centro AS (select * from centro where id in (select id from ${cc}_ctr)); \$\$" >> "aux/config.load"
+            echo "  DO \$\$ CREATE VIEW ${cc}_ctr with (security_invoker=on) AS (select distinct centro as id from concurso_anexo_centro where concurso='${c}' order by centro); \$\$" >> "aux/config.load"
+            echo "  DO \$\$ CREATE VIEW ${cc}_centro with (security_invoker=on) AS (select * from centro where id in (select id from ${cc}_ctr)); \$\$" >> "aux/config.load"
 
             for tb in $(sqlite3 "aux/db.sqlite" "SELECT name FROM sqlite_schema WHERE type='table';"); do
                 for col in $(sqlite3 "aux/db.sqlite" "select name from pragma_table_info('$tb') where name='centro'"); do
                 if [ "$tb" == "MACRO_ETAPA_CENTRO" ]; then
-            echo "  DO \$\$ CREATE VIEW ${cc}_${tb} AS (SELECT * FROM ${tb} WHERE ${col} IN (SELECT id FROM ${cc}_ctr) AND ${tb}.etapa IN (SELECT macro_etapa.id FROM macro_etapa WHERE macro_etapa.cuerpo IS NULL OR EXISTS (SELECT 1 FROM (SELECT unnest(string_to_array(macro_etapa.cuerpo, ' ')) AS palabra) macro_etapa_palabras WHERE macro_etapa_palabras.palabra IN (SELECT unnest(string_to_array(concurso.cuerpo, ' ')) FROM concurso WHERE concurso.id = '${cc}')))); \$\$" >> "aux/config.load"
+            echo "  DO \$\$ CREATE VIEW ${cc}_${tb} with (security_invoker=on) AS (SELECT * FROM ${tb} WHERE ${col} IN (SELECT id FROM ${cc}_ctr) AND ${tb}.etapa IN (SELECT macro_etapa.id FROM macro_etapa WHERE macro_etapa.cuerpo IS NULL OR EXISTS (SELECT 1 FROM (SELECT unnest(string_to_array(macro_etapa.cuerpo, ' ')) AS palabra) macro_etapa_palabras WHERE macro_etapa_palabras.palabra IN (SELECT unnest(string_to_array(concurso.cuerpo, ' ')) FROM concurso WHERE concurso.id = '${cc}')))); \$\$" >> "aux/config.load"
                 else
-            echo "  DO \$\$ CREATE VIEW ${cc}_${tb} AS (select * from ${tb} where ${col} in (select id from ${cc}_ctr)); \$\$" >> "aux/config.load"
+            echo "  DO \$\$ CREATE VIEW ${cc}_${tb} with (security_invoker=on) AS (select * from ${tb} where ${col} in (select id from ${cc}_ctr)); \$\$" >> "aux/config.load"
                 fi
                 done
             done
