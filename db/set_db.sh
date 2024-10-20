65a937341e9b51ee06119ee34c9deadfffd59eaf
s-nt-s <santos82h@gmail.com>
2024-10-20 22:02:36 +0200

    errata

--- db/set_db.sh
+++ db/set_db.sh
@@ -30,7 +30,7 @@ function mk_schema () {
             for tb in $(sqlite3 "aux/db.sqlite" "SELECT name FROM sqlite_schema WHERE type='table';"); do
                 for col in $(sqlite3 "aux/db.sqlite" "select name from pragma_table_info('$tb') where name='centro'"); do
                 if [ "$tb" == "MACRO_ETAPA_CENTRO" ]; then
-            echo "  DO \$\$ CREATE VIEW ${cc}_${tb} with (security_invoker=on) AS (SELECT * FROM ${tb} WHERE ${col} IN (SELECT id FROM ${cc}_ctr) AND ${tb}.etapa IN (SELECT macro_etapa.id FROM macro_etapa WHERE macro_etapa.cuerpo IS NULL OR EXISTS (SELECT 1 FROM (SELECT unnest(string_to_array(macro_etapa.cuerpo, ' ')) AS palabra) macro_etapa_palabras WHERE macro_etapa_palabras.palabra IN (SELECT unnest(string_to_array(concurso.cuerpo, ' ')) FROM concurso WHERE concurso.id = '${cc}')))); \$\$" >> "aux/config.load"
+            echo "  DO \$\$ CREATE VIEW ${cc}_${tb} with (security_invoker=on) AS (SELECT * FROM ${tb} WHERE ${col} IN (SELECT id FROM ${cc}_ctr) AND ${tb}.etapa IN (SELECT macro_etapa.id FROM macro_etapa WHERE macro_etapa.cuerpo IS NULL OR EXISTS (SELECT 1 FROM (SELECT unnest(string_to_array(macro_etapa.cuerpo, ' ')) AS palabra) macro_etapa_palabras WHERE macro_etapa_palabras.palabra IN (SELECT unnest(string_to_array(concurso.cuerpo, ' ')) FROM concurso WHERE concurso.id = '${c}')))); \$\$" >> "aux/config.load"
                 else
             echo "  DO \$\$ CREATE VIEW ${cc}_${tb} with (security_invoker=on) AS (select * from ${tb} where ${col} in (select id from ${cc}_ctr)); \$\$" >> "aux/config.load"
                 fi
